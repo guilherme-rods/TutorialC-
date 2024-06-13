5737a69520ff8bb7239408879d40a2d2bbe4a164
Guilherme Rodrigues <guilherme.g.rods@gmail.com>
2024-06-13 16:15:54 -0300

    Primeiro Commit

---
+++ Loja/data/LojaDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using loja.models;
+
+namespace loja.data
+{
+    public class LojaDbContext : DbContext
+    {
+        public LojaDbContext(DbContextOptions<LojaDbContext> options) : base(options) { }
+
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
