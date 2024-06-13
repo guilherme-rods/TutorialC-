96192530efabbf90a2dd53719aaddfef2e3f622f
Guilherme Rodrigues <guilherme.g.rods@gmail.com>
2024-06-13 20:56:51 -0300

    Finalização etapa 3 + criação da classe Cliente

--- Program.cs
+++ Program.cs
@@ -24,6 +24,44 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+//Endpoint para atualizar um produto existente:
+app.MapPut("/produtos/{id}", async (int id, LojaDbContext dbContext, Produto updateProduto) =>
+{
+    //Verifica se o produto existe na base, cdonforme o id informado
+    //Se o produto existir na base, será retornado para dentro do objeto existingProduto
+    var existingProduto = await dbContext.Produtos.FindAsync(id);
+    if(existingProduto == null){
+        return Results.NotFound($"Produto with ID {id} not found");
+    }
+
+    //Atualiza os dados do existingProduto:
+    existingProduto.Nome = updateProduto.Nome;
+    existingProduto.Preco = updateProduto.Preco;
+    existingProduto.Fornecedor = updateProduto.Fornecedor;
+
+    //Salva no banco de dados:
+    await dbContext.SaveChangesAsync();
+
+    //Retorna os dados para o cliente que invocou o endpoint:
+    return Results.Ok(existingProduto);
+
+});
+
+app.MapGet("/produtos/{id}", async (int id, LojaDbContext dbContext) =>
+{
+    var produto = await dbContext.Produtos.FindAsync(id);
+    if(produto == null){
+        return Results.NotFound($"Produto with ID {id} not found");
+    }
+    return Results.Ok(produto);
+});
+
+app.MapGet("/produtos", async (LojaDbContext dbContext) =>
+{
+    var produtos = await dbContext.Produtos.ToListAsync();
+    return Results.Ok(produtos);
+});
+
 app.MapPost("/createproduto", async (LojaDbContext dbContext, Produto newProduto) =>
 {
     dbContext.Produtos.Add(newProduto);
--- obj/Debug/net8.0/Loja.AssemblyInfo.cs
+++ obj/Debug/net8.0/Loja.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Loja")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5737a69520ff8bb7239408879d40a2d2bbe4a164")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a8f57a17189a1e8133361624532902493535448")]
 [assembly: System.Reflection.AssemblyProductAttribute("Loja")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Loja")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
